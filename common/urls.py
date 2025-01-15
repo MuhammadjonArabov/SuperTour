9e32cfa4ebbf7318de1d514e51df55cd99514c79
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-01-15 23:23:33 +0500

    update

--- common/urls.py
+++ common/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ApplicationViewSet, application_form_view
-from rest_framework import permissions
+from .views import ApplicationViewSet, application_form_view, ApplicationStatusStatsAPIView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
+
+router = DefaultRouter()
+router.register(r'api', ApplicationViewSet, basename='application')
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -18,15 +21,14 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
-router = DefaultRouter()
-router.register(r'api', ApplicationViewSet, basename='application')
-
 app_name = 'common'
 
 urlpatterns = [
     path('', application_form_view, name='application_form'),
 
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('api/applications/', ApplicationStatusStatsAPIView.as_view(), name='application-list'),
 
     path('api/', include(router.urls)),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import Application
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import ApplicationSerializers
 
 
@@ -21,3 +23,27 @@ def application_form_view(request):
 class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializers
+
+class ApplicationStatusStatsAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        new_count = Application.objects.filter(status='new').count()
+        old_count = Application.objects.filter(status='old').count()
+        total_count = Application.objects.count()
+
+        status_param = request.query_params.get('status', None)
+
+        if status_param:
+            applications = Application.objects.filter(status=status_param)
+        else:
+            applications = Application.objects.all()
+
+        serializer = ApplicationSerializers(applications, many=True)
+
+        return Response({
+            'status_counts': {
+                'new': new_count,
+                'old': old_count,
+                'total': total_count,
+            },
+            'applications': serializer.data
+        })
