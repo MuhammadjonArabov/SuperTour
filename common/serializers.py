53301f20e62a9382fe0f26b94c0db38110ef5af3
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-01-16 15:05:42 +0500

    super commit

--- common/serializers.py
+++ common/serializers.py
@@ -1,4 +1,3 @@
-from django.db.models.fields import return_None
 from rest_framework import serializers
 from .models import Application
 import re
--- common/views.py
+++ common/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+
 from .forms import ApplicationForm
 from rest_framework import viewsets, generics
 from .models import Application
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ApplicationSerializers
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 
 def application_form_view(request):
@@ -20,30 +24,49 @@ def application_form_view(request):
 
     return render(request, 'application_form.html', {'form': form})
 
+
 class ApplicationViewSet(viewsets.ModelViewSet):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializers
 
-class ApplicationStatusStatsAPIView(APIView):
-    def get(self, request, *args, **kwargs):
+
+class ApplicationPagination(PageNumberPagination):
+    page_size = 10
+    page_query_param = "page_size"
+    max_page_size = 100
+
+
+class ApplicationStatusStatsAPIView(generics.ListAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializers
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['status']
+    search_fields = ['full_name', 'phone_number', 'created_at']
+    pagination_class = ApplicationPagination
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response({
+                'status_counts': self.status_count(),
+                'applications': serializer.data
+            })
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            'status_counts': self.status_count(),
+            'applications': serializer.data
+        })
+
+    def status_count(self):
         new_count = Application.objects.filter(status='new').count()
         old_count = Application.objects.filter(status='old').count()
         total_count = Application.objects.count()
-
-        status_param = request.query_params.get('status', None)
-
-        if status_param:
-            applications = Application.objects.filter(status=status_param)
-        else:
-            applications = Application.objects.all()
-
-        serializer = ApplicationSerializers(applications, many=True)
-
-        return Response({
-            'status_counts': {
-                'new': new_count,
-                'old': old_count,
-                'total': total_count,
-            },
-            'applications': serializer.data
-        })
+        return {
+            "new": new_count,
+            "old": old_count,
+            "total": total_count,
+        }
