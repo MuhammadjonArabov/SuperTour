6c3679a052e76287cb9b4f50d8d1e82d0be0cd3d
Muhammadjon <arabovmuhammadjon1301@gmail.com>
2025-01-16 17:33:18 +0500

    update

--- common/urls.py
+++ common/urls.py
@@ -4,6 +4,8 @@ from .views import ApplicationViewSet, application_form_view, ApplicationStatusS
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'api', ApplicationViewSet, basename='application')
@@ -31,4 +33,4 @@ urlpatterns = [
     path('api/', include(router.urls)),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
-]
+] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
--- config/settings.py
+++ config/settings.py
@@ -145,12 +145,15 @@ USE_TZ = True
 
 
 
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
+STATIC_URL = 'static/'
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
